65d0f1c79047d8db9175af34d78688bef6403300
Sala G16 <CSalaG16@uniboyaca.edu.co>
2024-03-13 20:59:36 -0500

    js, html y css

---
+++ Ejercicio 2/js/index.js
@@ -0,0 +1,28 @@
+const Enviar=document.getElementById('Enviar');
+const form=document.forms['calendar'];
+
+//let cantidadDedias=(new Date(2024,5,0)).getDate();
+let cantidadDedias=(new Date(Anio,Mes,0)).getDate();
+let dias=[];
+for(let i=1;i<=cantidadDedias;i++){
+    dias.push(i);
+}
+
+console.log(dias);
+document.body.innerHTML=dias.map((value)=>{
+
+    return '<div>'+value+'</div>';
+}).join('');
+
+const validacionInput=()=>{
+    const anio=calendar['anio'].value;
+    const mes=calendar['mes'].value;
+    if(!anio){
+        alert('Deben ingresar numeros positivos');
+        return false;
+    }else if(!mes){
+        alert('Deben ingresar numeros positivos, mayores a 0 y menores a 13');
+        return false;
+    }
+
+}
