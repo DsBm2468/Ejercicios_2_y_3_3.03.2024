73b3fb94ed466f09ec4c29a2cb75f3d9905d938c
Sala D7 <CSalaD7@uniboyaca.edu.co>
2024-03-16 12:15:06 -0500

    cuadricula y ubicacion de mes

--- Ejercicio 2/js/index.js
+++ Ejercicio 2/js/index.js
@@ -1,22 +1,34 @@
-const Enviar=document.getElementById('Enviar');
-const form=document.forms['calendar'];
+const Enviar = document.getElementById('Enviar');
+const form = document.forms['calendar'];
+//const parrafos = document.getElementsByTagName('p');
+//const inputs=document.getElementsByClassName('num');
 
-let cantidadDedias=(new Date(2024,5,0)).getDate();
-//let cantidadDedias=(new Date(Anio,Mes,0)).getDate();
-let dias=[];
-for(let i=1;i<=cantidadDedias;i++){
-    dias.push(i);
+function generarDias(mes) {
+    let cantidadDedias = (new Date(2024, mes, 0)).getDate();
+    //let cantidadDedias=(new Date(Anio,Mes,0)).getDate();
+    let dias = [];
+    for (let i = 1; i <= cantidadDedias; i++) {
+        dias.push(i);
+    }
+
+    console.log(dias);
+    document.getElementById('resultados').innerHTML = dias.map((value) => {
+
+        return '<div class="ejemplo">' + value + '</div>';
+    }).join('');
 }
 
-console.log(dias);
-document.body.innerHTML=dias.map((value)=>{
+document.getElementById('mesSelect').addEventListener('change', (event) => {
+    console.log(event.target.value);
+    generarDias(event.target.value);
+});
 
-    return '<div>'+value+'</div>';
-}).join('');
+// SOLO DEJAR MES
 
-/**const validacionInput=()=>{
-    const anio=calendar['anio'].value;
-    const mes=calendar['mes'].value;
+
+/*function validacionInput(){
+    const anio=calendar['Anio'].value;
+    const mes=calendar['Mes'].value;
     if(!anio){
         alert('Deben ingresar numeros positivos');
         return false;
@@ -24,13 +36,28 @@ document.body.innerHTML=dias.map((value)=>{
            alert('Deben ingresar numeros positivos, mayores a 0 y menores a 13');
            return false;
         }
-}*/
+        return true;
+}
 
-/*var nombreMes=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+Enviar.addEventListener('click', () => {
+    if(validacionInput()){
+        const anio = parseInt(calendar['Anio'].value);
+        const mes = parseInt(calendar['Mes'].value);
+        parrafos[0].innerText='Año y mes: '+ resultado;
+    }
+});*/
+
+
+
+
+
+
+
+
+
+/**var nombreMes=["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
 var now = new Date();
 var day = now.getDate();
 var month = now.getMonth();
 var mesActual = month;
-var year = now.getFullYear();
-
-*/
+var year = now.getFullYear();*/
